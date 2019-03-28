1032a4d2c3af9a32f5030bf96543921fc95c545a
caiwenming <caiwenming@myhexin.com>
2019-03-28 10:11:51 +0800

    parse

--- wenzhan/spiders/meiwenzhan.py
+++ wenzhan/spiders/meiwenzhan.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import json
 
 class MeiwenzhanSpider(scrapy.Spider):
     name = 'meiwenzhan'
@@ -9,6 +9,6 @@ class MeiwenzhanSpider(scrapy.Spider):
     start_urls = [base_url]
 
     def parse(self, response):
-         douyu_data = json.loads(response.body)['data']
-         print('......................', douyu_data['title'])
+        douyu_data = json.loads(response.body)['data']
+        print('......................', douyu_data['title'])
         yield scrapy.Request(self.base_url, callback=self.parse)
