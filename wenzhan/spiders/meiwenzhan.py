3b62de87ad106f272b5c80a59040b59cfd875c0d
caiwenming <caiwenming@myhexin.com>
2019-03-28 20:24:54 +0800

    add file

--- wenzhan/spiders/meiwenzhan.py
+++ wenzhan/spiders/meiwenzhan.py
@@ -15,21 +15,21 @@ class MeiwenzhanSpider(scrapy.Spider):
    
     def parse(self, response):
         douyu_data = json.loads(response.body)['data']
-        author = pymysql.escape_string(douyu_data['author'])
-        title = pymysql.escape_string(douyu_data['title'])
-        digest = pymysql.escape_string(douyu_data['digest'])
-        content = pymysql.escape_string(douyu_data['content'])
-        print('......................', author)
-        print('......................', title)
-        print('......................', digest)
-        print('......................', self.alldays)
-        if (len(content) < 20000):
-            db = pymysql.connect('localhost', 'root', '897011805', 'yhj')
-            cursor = db.cursor()
-            sql = """INSERT INTO wenzhan values ('%s', '%s', '%s', '%s')"""%(author, title, digest, content)
-            cursor.execute(sql)
-            db.commit()
-            db.close()
+        if (len(douyu_data)>0):
+            author = pymysql.escape_string(douyu_data['author'])
+            title = pymysql.escape_string(douyu_data['title'])
+            digest = pymysql.escape_string(douyu_data['digest'])
+            content = pymysql.escape_string(douyu_data['content'])
+            print('......................', title)
+            if (len(content) < 20000):
+                db = pymysql.connect('localhost', 'root', '897011805', 'yhj')
+                cursor = db.cursor()
+                sql = """INSERT INTO wenzhan values ('%s', '%s', '%s', '%s')"""%(author, title, digest, content)
+                cursor.execute(sql)
+                db.commit()
+                db.close()
+        
+        
        
         self.offsize += 1
         yield scrapy.Request(self.base_url + str(self.alldays[self.offsize]), callback=self.parse)
