2d1e3f5ff356dfcbbd4ef4503831fc69248214b9
caiwenming <caiwenming@myhexin.com>
2019-03-28 15:37:11 +0800

    add time

--- wenzhan/getday.py
+++ wenzhan/getday.py
@@ -3,7 +3,7 @@ import datetime
 def get_nday_list(n):
     before_n_days = []
     for i in range(1, n + 1)[::-1]:
-        before_n_days.append(time.strptime(datetime.date.today() - datetime.timedelta(days=i), , "%Y%m%d"))
+        before_n_days.append((datetime.date.today() - datetime.timedelta(days=i)).strftime("%Y%m%d"))
     return before_n_days
 
 a = get_nday_list(7)
