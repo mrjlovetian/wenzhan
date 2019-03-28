363dac2a735410253afba7ec4f697f0aaabc6218
caiwenming <caiwenming@myhexin.com>
2019-03-28 11:11:41 +0800

    parse

--- wenzhan/spiders/meiwenzhan.py
+++ wenzhan/spiders/meiwenzhan.py
@@ -13,14 +13,18 @@ class MeiwenzhanSpider(scrapy.Spider):
 
     def parse(self, response):
         douyu_data = json.loads(response.body)['data']
-        print('......................', douyu_data['author'])
-        print('......................', douyu_data['title'])
-        print('......................', douyu_data['digest'])
-        print('......................', douyu_data['content'])
-
+        
+        author = pymysql.escape_string(ouyu_data['author'])
+        title = pymysql.escape_string(ouyu_data['title'])
+        digest = pymysql.escape_string(ouyu_data['digest'])
+        content = pymysql.escape_string(ouyu_data['content'])
+        print('......................', author)
+        print('......................', title)
+        print('......................', digest)
+        print('......................', content)
         db = pymysql.connect('localhost', 'root', '897011805', 'yhj')
         cursor = db.cursor()
-        sql = """INSERT INTO wenzhan values ('%s', '%s', '%s', '%s')"""%(douyu_data['author'], douyu_data['title'], douyu_data['digest'], douyu_data['content'])
+        sql = """INSERT INTO wenzhan values ('%s', '%s', '%s', '%s')"""%(author, title, digest, content)
         cursor.execute(sql)
         db.commit()
         db.close()
