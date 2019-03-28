503eeedbe94739b80bb81bcf81bb4dcaaffa9644
caiwenming <caiwenming@myhexin.com>
2019-03-28 10:40:53 +0800

    parse

--- wenzhan/spiders/meiwenzhan.py
+++ wenzhan/spiders/meiwenzhan.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import pymysql
 
 class MeiwenzhanSpider(scrapy.Spider):
     name = 'meiwenzhan'
@@ -12,6 +13,17 @@ class MeiwenzhanSpider(scrapy.Spider):
 
     def parse(self, response):
         douyu_data = json.loads(response.body)['data']
+        print('......................', douyu_data['author'])
         print('......................', douyu_data['title'])
+        print('......................', douyu_data['digest'])
+        print('......................', douyu_data['content'])
+
+        db = pymysql.connect('localhost', 'root', '897011805', 'yhj')
+        cursor = db.cursor()
+        sql = """INSERT INTO wenzhan values ('%s', '%s', '%s', '%s')"""%(douyu_data['author'], douyu_data['title'], douyu_data['digest'], douyu_data['content'])
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+
         self.offsize += 1
         yield scrapy.Request(self.base_url + str(self.offsize), callback=self.parse)
